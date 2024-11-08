0803881812703e491978c7ce826fb1fe02dac839
Eakawat Tantamjarik <eakawat2232@gmail.com>
2024-11-09 02:15:25 +0700

    Add task 3 sol

--- HyperNetwork/task/main.py
+++ HyperNetwork/task/main.py
@@ -3,4 +3,4 @@
 # Task 2
 # docker run -d --name hyper-postgres --env POSTGRES_PASSWORD=hyper2023 --env POSTGRES_USER=hyper --env POSTGRES_DB=hyper-db --network hyper-network -p 5432:5432 -v hyper-volume:/var/lib/postgresql/data postgres:15.3
 # Task 3
-# docker run -d
+# docker run -d --name hyper-adminer -p 8080:8080 --network hyper-network adminer:4.8.1
